b0f22ee8c586fdac1e0950aecac38e87ea5d88ce
rutvik2611 <rutvik2611@gmail.com>
2022-06-23 13:34:48 -0400

    more scripts

---
+++ scripts/2.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install -y jq
+
+
+jq --version
+
+echo "jq --------------------<"
+sudo apt update
+sudo apt install -y yq
+
+
+yq --version
+
+echo "yq --------------------<"
